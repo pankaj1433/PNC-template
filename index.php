3a824712bd817ca52b2dd4dd9ac58a31be9a6b1b
TTN <ttn@TTNs-MacBook-Pro.local>
2018-02-11 23:07:46 +0530

    contact form

--- index.php
+++ index.php
@@ -12,6 +12,8 @@
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="assets/css/main.css" />
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<meta property="og:title" content="PNC Learning">
+		<meta property="og:description" content="PNC Learning is a link of content development and content application in education field providing learning solutions and applications for enhancement of learning capabilities for students">
 	</head>
 	<body class="homepage">
 		<div id="page-wrapper">
@@ -47,9 +49,10 @@
 				<section id="banner">
 					<header>
 						<h2><strong>About Us</strong>.</h2>
-							<p>PNC Learning is a link of <strong> development and content application </strong>in education field
-								providing learning solutions and applications for enhancement of learning capabilities for
-								students... </p>
+							<p>PNC Learning is a link of 
+								<strong> development and content application </strong>
+								in education field providing learning solutions 
+								and applications for enhancement of learning capabilities for students... </p>
 							<footer>
 								<a href="about.html" class="button">Continue Reading</a>
 							</footer>
@@ -57,7 +60,7 @@
 				</section>
 
 				<!-- Carousel -->
-				<section class="carousel">
+				<!-- <section class="carousel">
 					<div class="reel">
 
 						<article>
@@ -65,28 +68,28 @@
 							<header>
 								<h3><a href="#">Support Material</a></h3>
 							</header>
-							<!-- <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p> -->
+							
 						</article>
 						<article>
 							<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
 							<header>
 								<h3><a href="#">OMR Based test</a></h3>
 							</header>
-							<!-- <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p> -->
+							
 						</article>
 						<article>
 							<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
 							<header>
 								<h3><a href="#">Question Banks</a></h3>
 							</header>
-							<!-- <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p> -->
+							
 						</article>
 						<article>
 							<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
 							<header>
 								<h3><a href="#">Test Series</a></h3>
 							</header>
-							<!-- <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p> -->
+							
 						</article>
 
 						<article>
@@ -94,31 +97,32 @@
 							<header>
 								<h3><a href="#">Web Based Testing</a></h3>
 							</header>
-							<!-- <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p> -->
+							
+							
 						</article>
 						<article>
 							<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
 							<header>
 								<h3><a href="#">APP based Testing</a></h3>
 							</header>
-							<!-- <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p> -->
+							
 						</article>
 						<article>
 							<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
 							<header>
 								<h3><a href="#">P2P platforms</a></h3>
 							</header>
-							<!-- <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p> -->
+							
 						</article>
 						<article>
 							<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
 							<header>
 								<h3><a href="#">Google &amp; Apple App’s</a></h3>
 							</header>
-							<!-- <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p> -->
+							
 						</article>
 					</div>
-				</section>
+				</section> -->
 
 			<!-- Main -->
 				<div class="wrapper style2">
@@ -128,9 +132,11 @@
 						<header>
 							<h2><a href="#">Carriers</a></h2>
 							<p>
-								We’re on the road to expanding opportunities for everyone. From expanding our team to
-								implementing best employer practices. If interested please drop a mail with your resume
-								at: <strong>Opport@pnclearning.co.in</strong>
+									We’re on the road to expanding opportunities
+									 for everyone. From expanding our team to
+									  implementing best employer practices. 
+									  If interested please drop a mail with your resume at:
+									<strong>hr@pnclearning.co.in</strong>
 							</p>
 						</header>
 					</article>
@@ -174,14 +180,14 @@
 										<header>
 											<h3>PNC</h3>
 										</header>
-										<ul class="icons">
+										<!-- <ul class="icons">
 											<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
 											<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
 											<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
 											<li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
 											<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
 											<li><a href="#" class="icon fa-linkedin"><span class="label">Linkedin</span></a></li>
-										</ul>
+										</ul> -->
 									</section>
 
 								<!-- Copyright -->
